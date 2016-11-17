bbaaa312b9c2f252ba3ee6055511c08dae3252dd
Albin Bernhardsson <catscratchesp@gmail.com>
2016-11-17 15:21:50 +0100

    Print help

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include <cstring>
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char* argv[]) {
-    std::cout << "Hello, world!" << std::endl;
+    // Print help
+    if (argc == 1 || strcmp(argv[0], "--help") == 0) {
+        cout << "GLSLPreprocessor is a simple preprocessor for GLSL files." << endl
+             << "Usage:" << endl
+             << "GLSLPreprocessor input output" << endl;
+        return 0;
+    }
     
     return 0;
 }
